34cf95655d2ca14de82f2645cf554ddb06469787
Giovana Valim <104873469+givalim@users.noreply.github.com>
2024-02-25 16:42:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,63 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+const menuBtn = document.getElementById("menu-btn"); // obtem o elemento do botão de menu pelo seu ID.
+const navLinks = document.getElementById("nav-links");// obtem a lista de links de nav. pelo ID
+const menuBtnIcon = menuBtn.querySelector("i"); //obtem o icone dentro do botao de menu
 
 menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
+    navLinks.classList.toggle("open"); //quando o menu é clicado, a função é executada.
 
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line"); //o icone também é alterado dependendo se o menu está aberto ou não.
 });
 
 navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
-const headerImage = document.querySelector(".header__image");
+const headerImage = document.querySelector(".h-image"); // obtem o elemento da imagem da classe h-container
 headerImage.addEventListener(
-  "animationend",
-  (e) => {
-    setTimeout(() => {
-      headerImage.classList.add("reveal");
-    });
-  },
-  { once: true }
+    "animationend",
+    (e) => {
+        setTimeout(() => {
+            headerImage.classList.add ("reveal"); // depois de um "delay" mostra a imagem.
+        });
+    },
+    {once: true }//chamado somente uma vez.
 );
 
-const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000,
+const scrollRevealOption = { // especifica a distancia, origem e a duração da animação de revelação.
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
 };
 
-ScrollReveal().reveal(".header__content h1", {
-  ...scrollRevealOption,
-  delay: 1500,
-});
-ScrollReveal().reveal(".header__content h2", {
-  ...scrollRevealOption,
-  delay: 2000,
-});
-ScrollReveal().reveal(".header__content p", {
-  ...scrollRevealOption,
-  delay: 2500,
-});
-ScrollReveal().reveal(".header__content div", {
-  ...scrollRevealOption,
-  delay: 3000,
+ScrollReveal().reveal(".h-content h1", { //usa a biblioteca ScrollReveal.js para aplicar o efeito de revelação
+    ...scrollRevealOption,
+    delay: 1500,
 });
 
-ScrollReveal().reveal(".header .nav__links", {
-  delay: 3500,
-});
+ScrollReveal().reveal(".h-content h2", {
+    ...scrollRevealOption,
+    delay: 2000,
+  });
+  ScrollReveal().reveal(".h-content p", {
+    ...scrollRevealOption,
+    delay: 2500,
+  });
+  ScrollReveal().reveal(".h-content div", {
+    ...scrollRevealOption,
+    delay: 3000,
+  });
+  
+  ScrollReveal().reveal(".header .nav__links", {
+    delay: 3500,
+  });
+
